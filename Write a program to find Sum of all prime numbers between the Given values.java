14fe18f69f9ad26db60a070de1cab785a676c703
Nune Siva Kumar <sivakumarnune978@gmail.com>
2026-03-03 16:33:38 +0530

    Create Write a program to find Sum of all prime numbers between the Given values.java

---
+++ Write a program to find Sum of all prime numbers between the Given values.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String []args){
+    Scanner Sc =new Scanner(System.in);
+    int a = Sc.nextInt();
+    int b = Sc.nextInt();
+    int fc =0;
+    int sum=0;
+    if(a<1  || b<1)
+    {
+        System.out.print("Invalid Inputs");
+    }
+    else
+    {
+        for(int i=a+1;i<b;i++)
+        {
+            fc=0;
+            for(int j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    fc++;
+                }
+            }
+            if(fc==2)
+            {
+                sum=sum+i;
+            }
+        }
+        System.out.print(sum);
+    }
+    
+    }
+}
