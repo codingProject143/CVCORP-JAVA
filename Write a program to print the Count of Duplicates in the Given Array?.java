e50bb1cea08e3df11f36a48f173f3095d49fdf21
Nune Siva Kumar <sivakumarnune978@gmail.com>
2026-03-03 19:26:39 +0530

    Create Write a program to print the Count of Duplicates in the Given Array?.java

---
+++ Write a program to print the Count of Duplicates in the Given Array?.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class DuplicateCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in):
+        int size;
+        try {
+            size = scanner.nextInt();
+            if (size <= 5) {
+                System.out.println("Invalid Array Size.");
+                return;
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid Array Size.");
+            return;
+        }
+        
+        int[] arr = new int[size];
+        try {
+            for (int i = 0; i < size; i++) {
+                arr[i] = scanner.nextInt();
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid Array Elements.");
+            return;
+          
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : arr) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+       
+        int duplicateCount = 0;
+        for (int count : frequencyMap.values()) {
+            if (count > 1) {
+                duplicateCount++;
+            }
+        }
+      
+        if (duplicateCount == 0) {
+            System.out.println("No Duplicates exists");
+        } else {
+            System.out.println(duplicateCount);
+        }
+    }
+}
