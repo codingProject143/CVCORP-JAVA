f6acd9347e41b2218e34c3119aaf947568683063
Nune Siva Kumar <sivakumarnune978@gmail.com>
2026-03-03 17:16:38 +0530

    Create Find the nth term value in the geometric progression series by taking input of 1st term(a), common Ratio(r) and nth term.java

---
+++ Find the nth term value in the geometric progression series by taking input of 1st term(a), common Ratio(r) and nth term.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int r = sc.nextInt();
+        int n = sc.nextInt();
+        
+        if(n<=0)
+        {
+            System.out.print("InValid Input.");
+        }
+        else
+        {
+            int ntr=a*(int)Math.pow(r,(n-1));
+            System.out.print("Last term value is : "+ntr+".");
+        }
+    }
+}
