082f6eda14792baf7fa6014822fb28f1e6036218
Nune Siva Kumar <sivakumarnune978@gmail.com>
2026-03-03 18:57:39 +0530

    Create Write a Program 5-1 to Print the Following Pattern?.java

---
+++ Write a Program 5-to Print the Following Pattern?.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+       
+        int n = sc.nextInt();
+        if(n<=0)
+        {
+            System.out.print("Invalid Input");
+        }
+        else
+        {
+            
+           for(int i=n;i>=1;i--)
+           {int k=n;
+               for(int j=i;j>=1;j--)
+               {
+                   System.out.print(k--+" ");
+               }
+               System.out.println();
+           }
+            
+        }
+    }
+}
